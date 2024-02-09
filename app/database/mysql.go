4ec072ca8208cbfef9849faa379b855295db4ccc
anggeldwi <dwimirandaa@gmail.com>
2024-02-09 22:31:21 +0700

    add post tour

--- app/database/mysql.go
+++ app/database/mysql.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"my-tourist-ticket/app/configs"
 	cd "my-tourist-ticket/features/city/data"
+	td "my-tourist-ticket/features/tour/data"
 	ud "my-tourist-ticket/features/user/data"
 
 	"gorm.io/driver/mysql"
@@ -24,7 +25,6 @@ func InitDBMysql(cfg *configs.AppConfig) *gorm.DB {
 }
 
 func InitMigrate(db *gorm.DB) {
-	db.AutoMigrate(&ud.User{})
-	db.AutoMigrate(&cd.City{})
+	db.AutoMigrate(&ud.User{}, &cd.City{}, &td.Tour{})
 
 }
---
+++ features/tour/data/model.go
@@ -0,0 +1,38 @@
+package data
+
+import (
+	_cityData "my-tourist-ticket/features/city/data"
+	"my-tourist-ticket/features/tour"
+	_userData "my-tourist-ticket/features/user/data"
+
+	"gorm.io/gorm"
+)
+
+type Tour struct {
+	gorm.Model
+	CityId      uint
+	UserId      uint
+	TourName    string
+	Description string
+	Image       string
+	Thumbnail   string
+	Addres      string
+	Latitude    float64 `gorm:"type:decimal(10,8)"`
+	Longitude   float64 `gorm:"type:decimal(11,8)"`
+	User        _userData.User
+	City        _cityData.City
+}
+
+func CoreToModel(t tour.Core) Tour {
+	return Tour{
+		CityId:      t.CityId,
+		UserId:      t.UserId,
+		TourName:    t.TourName,
+		Description: t.Description,
+		Image:       t.Image,
+		Thumbnail:   t.Thumbnail,
+		Addres:      t.Address,
+		Latitude:    t.Latitude,
+		Longitude:   t.Longitude,
+	}
+}
